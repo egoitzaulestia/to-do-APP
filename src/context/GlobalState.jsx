ddf75fa835965aad671c7bd68482616633578f14
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-08 15:17:21 +0200

    GobalState: getTask done

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -1,8 +1,32 @@
-import React, {createContext} from "react";
-import axios from axios
+import React, { createContext, useReducer } from "react";
+import axios from "axios";
+import AppReducer from "./AppReducer";
 
 const initialState = {
-    tasks: []
-}
+  tasks: [],
+};
 
-expert const GlobalContext = createContext(initialState)
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  const getTasks = async () => {
+    const res = await axios.get("http://localhost:3000/tasks");
+
+    dispatch({
+      type: "GET_TASKS",
+      payload: res.data.tasks,
+    });
+  };
+  return (
+    <GlobalContext.Provider
+      value={{
+        tasks: state.tasks,
+        getTasks,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
