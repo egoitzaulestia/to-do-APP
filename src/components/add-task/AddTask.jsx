c4d703bb254ba9152bc0185cbc4b55222ed1da2c
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-08 17:42:41 +0200

    AddTask component created

--- src/components/add-task/AddTask.jsx
+++ src/components/add-task/AddTask.jsx
@@ -1,5 +1,25 @@
+import { useContext, useState } from "react";
+import { GlobalContext } from "../../context/GlobalState";
+
 const AddTask = () => {
-  return <div>AddTask</div>;
+  const [title, setTitle] = useState("");
+  const { addTask } = useContext(GlobalContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("title", title);
+    addTask({ title });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        name="title"
+      />
+      <button type="submit">Add task</button>
+    </form>
+  );
 };
 
 export default AddTask;
