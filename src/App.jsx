6a5e2bfd1c31e4025c423bce8cb247e553d8cf08
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-08 18:37:24 +0200

    react-router-dom in use. BrowserRouter, Routes, adn Route ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import TaskList from "./components/task-list/TaskList";
+import EditTask from "./components/edit-task/EditTask";
+
 import { GlobalProvider } from "./context/GlobalState";
 import "./App.css";
 
 function App() {
   return (
-    <GlobalProvider>
-      <TaskList />
-    </GlobalProvider>
+    <BrowserRouter>
+      <GlobalProvider>
+        <Routes>
+          <Route path="/" element={<TaskList />} />
+          <Route path="/:_id" element={<EditTask />} />
+        </Routes>
+      </GlobalProvider>
+    </BrowserRouter>
   );
 }
 
