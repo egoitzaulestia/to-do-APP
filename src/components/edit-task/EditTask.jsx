3a40fdb6d425281f7568dd413284627c7576dbb2
egoitzaulestia <egoitzaulestia@gmail.com>
2025-07-08 19:47:17 +0200

    editTask useEffect and form created

--- src/components/edit-task/EditTask.jsx
+++ src/components/edit-task/EditTask.jsx
@@ -5,15 +5,30 @@ import { GlobalContext } from "../../context/GlobalState";
 const EditTask = () => {
   const { _id } = useParams();
   const { task, getTask } = useContext(GlobalContext);
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     getTask(_id);
   }, []);
 
+  useEffect(() => {
+    setTitle(task.title);
+  }, [task.title]);
+
   console.log("id", _id);
   console.log("task", task.task);
 
-  return <div>EditTask</div>;
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title || ""}
+        name="title"
+      />
+      <button type="submit">Edit task</button>
+    </form>
+  );
 };
 
 export default EditTask;
